a01f485fb099b402a158413afe5aeacdf3a24938
linli <906349355@qq.com>
2015-04-07 23:01:03 +0600

    5 finished

--- src/main/java/camt/se331/shoppingcart/config/DatabaseInitializationBean.java
+++ src/main/java/camt/se331/shoppingcart/config/DatabaseInitializationBean.java
@@ -7,6 +7,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 
+import java.util.Arrays;
+
 /**
  * Created by LIN LI on 4/7/2015.
  */
@@ -24,7 +26,13 @@ public class DatabaseInitializationBean implements InitializingBean {
                 new Product(3l,"Mac pro","Mac book interim",44000.00),
                 new Product(4l,"Candle","Use for lightenup the world",10.00),
                 new Product(5l,"Bin","User for what ?",200.00),
-                
-        }
+                new Product(6l,"Telephone","call the others",150.00),
+                new Product(7l,"iPhone","what it is ?",26000.00),
+                new Product(8l,"Galaxy Note 4","Who still use this ?",24000.00),
+                new Product(9l,"AngularJS","We hate it",2000.00),
+                new Product(10l,"Mazda","Very handsome guy use this",300000.00)
+        };
+        productRepository.save(Arrays.asList(initProduct));
+        productRepository.save(new Product(1l,"Kindle","the good book reader",6900.00));
     }
 }
